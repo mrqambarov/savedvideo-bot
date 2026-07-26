ff1e9360c74322202eeb369c7e20e63224e84be7
mrqambarov <mrqambarov@github.com>
2026-07-26 15:58:10 +0500

    fix: set base URL to /movies/ and API path to /movies/api for reverse proxy compatibility

--- movie-client/src/App.jsx
+++ movie-client/src/App.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const API_BASE = window.location.origin.includes('localhost') || window.location.origin.includes('127.0.0.1') 
   ? 'http://localhost:5001/api' 
-  : '/api';
+  : '/movies/api';
 
 // Axios Authorization Header Interceptor
 axios.interceptors.request.use((config) => {
